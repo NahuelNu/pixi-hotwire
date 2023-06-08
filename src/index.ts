5e4b9b3c1ecf3b072b278de854adf0b0cf78441a
Nahuel Nuñez <83452993+NahuelNu@users.noreply.github.com>
2023-06-07 21:06:57 -0300

    Resize
    Tamaño y márgenes se actualizan al redimensionar la pantalla

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,33 @@ Assets.load(['Casco','Bicho']).then(()=>{
 	const casco: Sprite = Sprite.from('Casco');
 	casco.scale.set(0.35);
 	casco.position.set(93,-4);
-	casco.rotation=-Math.PI/9;
+	casco.rotation = -Math.PI/9;
 
 	const bichoandcasco: Container = new Container();
-	bichoandcasco.position.set(100,100);
+	bichoandcasco.position.set(192,92);
 
 	app.stage.addChild(bichoandcasco);
 	bichoandcasco.addChild(bicho);
 	bichoandcasco.addChild(casco);
 })
 
+addEventListener("resize",()=>{
+	let scaleX = window.innerWidth /app.screen.width;
+	let scaleY = window.innerHeight /app.screen.height;
+	let scale = Math.min(scaleX,scaleY);
+
+	let gameWidth = Math.round(scale * app.screen.width);
+	let gameHeight = Math.round(scale * app.screen.height);
+
+	app.view.style!.width= gameWidth + "px";
+	app.view.style!.height= gameHeight + "px";
+
+	let marginHorizontal = Math.floor((window.innerWidth - gameWidth)/2);  
+	let marginVertical = Math.floor((window.innerHeight-gameHeight)/2);
+
+	(app.view.style as any).marginLeft= marginHorizontal+"px";
+	(app.view.style as any).marginTop= marginVertical+"px";
+	
+});
+
+dispatchEvent(new Event("resize"));
